68c64f8ee3e8a0da20915762c5c7ca63db781f35
長内福智 <yoshito@osanaifukuchis-MacBook-Pro.local>
2021-11-03 00:09:30 +0900

    Avoid duplicate folder creation for the same author name.

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ def main():
     bookmarks = users_data['illusts']
 
     # ダウンロード
-    #for bookmark in bookmarks:
-        #download_image(bookmark)
+    for bookmark in bookmarks:
+        download_image(bookmark)
 
     next_url = users_data['next_url']
     next_qs = aapi.parse_qs(next_url)
@@ -63,9 +63,10 @@ def download_image(work_info):
     id = work_info.id
     title = work_info.title.replace("/", "-")
     author = work_info.user.name.replace("/", "-")
+    author_id = work_info.user.id
 
     # ダウンロード先のフォルダ作成
-    savepath = f"./pixiv_images/{author}/{id}"
+    savepath = f"./pixiv_images/{author}({author_id})/{title}({id})"
     if not os.path.exists(savepath):
         os.makedirs(savepath)
 
@@ -76,7 +77,7 @@ def download_image(work_info):
         if is_manga(work_info):
             pages = work_info['meta_pages']
             for i, page in enumerate(pages):
-                print(f"Downloading {title}({author}) page:{i}")
+                print(f"[INFO] Downloading {title}({author}) page:{i}")
                 aapi.download(page['image_urls']['original'], path = savepath)
                 sleep(1)
 
@@ -89,54 +90,6 @@ def download_image(work_info):
 
     else:
         print(f'[INFO] {title}({author}) id:{id} already exists')
-
-# def download_image(users_data, count, allCount):
-#     # イラストの数だけ繰り返す
-#     ilustNum = len(users_data.illusts)
-#     for illust in users_data.illusts[:ilustNum]:
-#         author = illust.user.name.replace("/", "-")
-
-#         # ダウンロードフォルダなかったら作る
-#         if not os.path.exists("./pixiv_images/" + author):
-#             os.makedirs("./pixiv_images/" + author)
-
-#         # 保存先の指定
-#         savepath = "./pixiv_images/" + author
-
-#         # 保存
-#         aapi.download(illust.image_urls.large, path = savepath)
-#         print(str(allCount) + "枚目の画像: " + str(author)+"  " + str(illust.title))
-#         count += 1
-#         allCount += 1
-#         sleep(1)
-
-#         #30回目以降
-#         if count > 30:
-#             next_url = users_data.next_url
-#             next_qs = aapi.parse_qs(next_url)
-#             # users_dataに30以降のjsonデータを再代入
-#             users_data = aapi.user_bookmarks_illust(**next_qs)
-#             count = 1
-#             download_image(users_data, count, allCount)
-
-# mkdirExceptExist = lambda path: "" if os.path.exists(path) else os.mkdir(path)
-
-# # 漫画の場合
-# if work_info.is_manga:
-#     mkdirExceptExist(saving_direcory_path + "manga/" + work_title) # 保存用フォルダがない場合は生成
-
-#     manga_info = api.works(work_info.id)
-#     for page_no in range(0, manga_info.response[0].page_count):
-#         page_info = manga_info.response[0].metadata.pages[page_no]
-#         num = str(page_no) if len(str(page_no)) > 1 else "0" + str(page_no)
-#         aapi.download(page_info.image_urls.large, path=saving_direcory_path + "manga/" + work_title, name=num+".jpg")
-#         sleep(3)
-# # イラストの場合
-# else:
-#     aapi.download(work_info.image_urls.large, path=saving_direcory_path + "illust", name=work_title+".jpg")
-#     sleep(3)
-
-# download_image(users_data, count, all_count)
 
 if __name__ == '__main__':
     main()
